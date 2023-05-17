b215d52c3a6ca84c30fbbb8a95f9dad00c0c17ab
Marie <marie.borgmeier@urs-os.de>
2023-05-17 19:34:05 +0200

    VD 51

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -24,19 +24,20 @@ class SessionsController extends Controller
 
         // attempt to authenticate and log in the user
         // based on the provided credentials
-        if (auth()->attempt($attributes))
+        if (! auth()->attempt($attributes))
         {
-            // session fixation
-            session()->regenerate();
-            // redirect with a success flash message
-            return redirect('/')->with('success', 'You are logged in!');
+            // auth failed
+            throw ValidationException::withMessages([
+                'email' => 'Your provided credentials could not be verified.'
+            ]);
+            // another way to do it!!!
+            // return back()->withErrors(['email' => 'Your provided credentials could not be verified.']);
         }
 
-        // auth failed
-        throw ValidationException::withMessages([
-            'email' => 'Your provided credentials could not be verified.']);
-        // another way to do it!!!
-        // return back()->withErrors(['email' => 'Your provided credentials could not be verified.']);
+        // session fixation
+        session()->regenerate();
+        // redirect with a success flash message
+        return redirect('/')->with('success', 'You are logged in!');
     }
 
     public function destroy()
