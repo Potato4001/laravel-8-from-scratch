d9f805d8cca2f72d84d71011ec7bb931a9df8168
Marie <marie.borgmeier@urs-os.de>
2023-05-28 14:26:28 +0200

    VD 54

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\Post;
+use App\Models\User;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comment>
@@ -19,8 +20,8 @@ class CommentFactory extends Factory
     {
         return [
             'post_id' => Post::factory(),
-            'user_id' => Post::factory(),
-            'body' => $this->fake()->paragraph()
+            'user_id' => User::factory(),
+            'body' => $this->faker->paragraph()
         ];
     }
 }
